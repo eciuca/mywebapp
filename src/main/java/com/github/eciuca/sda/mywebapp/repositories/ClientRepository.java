59b7aec186c30f2ccb60b596a92443035aeb8c7a
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2019-03-02 14:20:39 +0200

    Remove @Query

--- src/main/java/com/github/eciuca/sda/mywebapp/repositories/ClientRepository.java
+++ src/main/java/com/github/eciuca/sda/mywebapp/repositories/ClientRepository.java
@@ -2,7 +2,6 @@ package com.github.eciuca.sda.mywebapp.repositories;
 
 import com.github.eciuca.sda.mywebapp.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +9,5 @@ import java.util.List;
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
-    @Query(value = "SELECT c FROM clients c WHERE c.name = :name")
     List<Client> findAllByName(String name);
 }
