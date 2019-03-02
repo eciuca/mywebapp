9d8404a667ee182c4ffa46c62aa9eec07cb98566
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2019-03-02 13:06:00 +0200

    Disable security for all pages

---
+++ src/main/java/com/github/eciuca/sda/mywebapp/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.github.eciuca.sda.mywebapp.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**")
+                .permitAll();
+
+//        super.configure(http);
+//                .antMatchers("/css/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .permitAll();
+    }
+}
