f056a0b79fab96ed6fcf46b248dfc3bb8bb4d790
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2019-03-03 10:57:15 +0200

    Move and rename SecurityConfig to WebSecurityConfig

--- src/main/java/com/github/eciuca/sda/mywebapp/auth/WebSecurityConfig.java
+++ src/main/java/com/github/eciuca/sda/mywebapp/auth/WebSecurityConfig.java
@@ -1,11 +1,11 @@
-package com.github.eciuca.sda.mywebapp.config;
+package com.github.eciuca.sda.mywebapp.auth;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
